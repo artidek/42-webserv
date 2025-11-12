a478a36c0ead38f5855d347dffed281ab440e329
aobshatk <obshatko.artem@gmail.com>
2025-11-13 00:22:44 +0100

    finished parsing of the config file

--- includes/configParser.hpp
+++ includes/configParser.hpp
@@ -6,7 +6,7 @@
 /*   By: aobshatk <aobshatk@42warsaw.pl>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 12:37:36 by aobshatk          #+#    #+#             */
-/*   Updated: 2025/11/12 11:33:02 by aobshatk         ###   ########.fr       */
+/*   Updated: 2025/11/12 21:54:58 by aobshatk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,23 +23,29 @@ class configParser
 	private:
 		static std::stack<std::string> tokens;
 		static std::string flattened;
+		static std::string blockProp;
 		static bool blockEnd;
-		static std::vector<std::string> langBlocks;
-		static std::vector<std::string> langBlockLocation;
+		static std::vector<serverConfig> hosts;
+		static std::map<std::string, void(*)(std::stack<std::string>&)>blockNames;
 		configParser(void);
 		configParser(configParser const &copy);
 		configParser &operator=(configParser const &copy);
 		~configParser(void);
 		static void flatten(std::ifstream const &file);
 		static void tokenize(void);
-		static void fillHostConf(void);
-		static void fillErrPg(void);
-		static void fillRoute(void);
-		static void fillLoc(void);
-		static void fillCgiConf(void);
+		static void fillHostConf(std::stack<std::string> &blockTokens);
+		static void fillErrPg(std::stack<std::string> &blockTokens);
+		static void fillRoute(std::stack<std::string> &blockTokens);
+		static void fillLoc(std::stack<std::string> &blockTokens);
+		static void fillCgiConf(std::stack<std::string> &blockTokens);
 		static void listToken(int &i);
+		static void parseBlock(void);
+		static void parseList(std::stack<std::string> &blockTokens);
+		static void startBlock(void);
+		static void checkBlock(std::stack<std::string>&blockTokens);
+		static void initBlockNames(void);
 	public:
-		static std::vector<serverConfig> parseConfig(std::string confFile);
+		static void parseConfig(std::string confFile);
 };
 
 #endif
--- src/configParser.cpp
+++ src/configParser.cpp
@@ -1,47 +1,51 @@
 #include "../includes/configParser.hpp"
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 
 std::stack<std::string> configParser::tokens;
+std::vector<serverConfig> configParser::hosts;
+std::map<std::string,
+	void (*)(std::stack<std::string> &)> configParser::blockNames;
 std::string configParser::flattened;
+std::string configParser::blockProp;
 bool configParser::blockEnd = false;
 
-bool isCurlBr(unsigned char c)
+bool	isCurlBr(unsigned char c)
 {
 	if (c == '{' || c == '}')
-		return true;
-	return false;
+		return (true);
+	return (false);
 }
 
-void checkExt(std::string confFile)
+void	checkExt(std::string confFile)
 {
 	std::stringstream ss(confFile);
 	std::string name, extension;
-
 	if (std::getline(ss, name, '.') && std::getline(ss, extension))
 	{
 		if (extension == "conf")
-			return;
+			return ;
 		else
 			throw errorHandler(WRONG_EXT, extension);
 	}
 	throw errorHandler(WRONG_EXT, "");
 }
 
-void configParser::flatten(std::ifstream const & file)
+void configParser::flatten(std::ifstream const &file)
 {
 	std::ostringstream buffer;
 	buffer << file.rdbuf();
-
 	if (buffer.str().empty())
 		throw errorHandler(CONFIG_EMPTY);
 	flattened = buffer.str();
+	flattened.erase(std::remove(flattened.begin(), flattened.end(), '\n'),
+		flattened.end());
 }
 
 void configParser::listToken(int &i)
 {
 	std::string token;
-
 	for (; flattened[i]; i++)
 	{
 		if (flattened[i] == ']')
@@ -52,52 +56,234 @@ void configParser::listToken(int &i)
 				token.clear();
 			}
 			token += flattened[i];
+			i++;
 			tokens.push(token);
-			return;
+			return ;
 		}
 		if (flattened[i] == ',' && !token.empty())
 		{
 			tokens.push(token);
 			token.clear();
 		}
+		else if (flattened[i] == '[' || flattened[i] == '{'
+			|| flattened[i] == '}')
+			throw errorHandler(MISSING_TOKEN, "]");
 		else if (!isspace(static_cast<unsigned char>(flattened[i])))
-			token  += flattened[i];
+			token += flattened[i];
 	}
 }
 
 void configParser::tokenize(void)
 {
 	std::string token;
-	for (int i = 0; flattened[i]; i++)
+	try
 	{
-		if(isCurlBr(flattened[i]))
+		for (int i = 0; flattened[i]; i++)
 		{
-			token += flattened[i];
-			tokens.push(token);
-			token.clear();
+			if (isCurlBr(flattened[i]) && token.empty())
+			{
+				token += flattened[i];
+				tokens.push(token);
+				token.clear();
+			}
+			else if (flattened[i] == '[')
+			{
+				token += flattened[i];
+				tokens.push(token);
+				token.clear();
+				i++;
+				listToken(i);
+			}
+			else if(flattened[i] == ']')
+				throw errorHandler(MISSING_TOKEN, "[");
+			else if (!isspace(static_cast<unsigned char>(flattened[i])) && !isCurlBr(flattened[i]))
+				token += flattened[i];
+			else if (isspace(static_cast<unsigned char>(flattened[i]))
+				&& !token.empty())
+			{
+				tokens.push(token);
+				token.clear();
+			}
+			else if (flattened[i] == '{')
+			{
+				tokens.push(token);
+				tokens.push("{");
+				token.clear();
+				i++;
+			}
 		}
-		else if (flattened[i] == '[')
-		{
-			token += flattened[i];
-			tokens.push(token);
-			token.clear();
-			i++;
-			listToken(i);
-		}	
-		else if (!isspace(static_cast<unsigned char>(flattened[i])))
-			token += flattened[i];
-		else if (isspace(static_cast<unsigned char>(flattened[i])) && !token.empty())
-		{
-			tokens.push(token);
-			token.clear();
-		}
+	}
+	catch (const std::exception &e)
+	{
+		throw errorHandler(std::string(e.what()));
 	}
 }
 
-std::vector<serverConfig> configParser::parseConfig(std::string confFile)
+void configParser::startBlock(void)
 {
-	std::vector<serverConfig> res;
+	if (tokens.top() == "}")
+	{
+		tokens.pop();
+		if (tokens.top() == "}")
+		{
+			blockEnd = true;
+			tokens.pop();
+			return ;
+		}
+	}
+	throw errorHandler(MISSING_TOKEN, "}");
+}
 
+void configParser::fillHostConf(std::stack<std::string> &blockTokens)
+{
+	while(!blockTokens.empty())
+		blockTokens.pop();
+}
+
+void configParser::fillErrPg(std::stack<std::string> &blockTokens)
+{
+	while(!blockTokens.empty())
+		blockTokens.pop();
+}
+
+void configParser::fillRoute(std::stack<std::string> &blockTokens)
+{
+	while(!blockTokens.empty())
+		blockTokens.pop();
+}
+
+void configParser::fillLoc(std::stack<std::string> &blockTokens)
+{
+	while(!blockTokens.empty())
+		blockTokens.pop();
+}
+
+void configParser::fillCgiConf(std::stack<std::string> &blockTokens)
+{
+	while(!blockTokens.empty())
+		blockTokens.pop();
+}
+
+void configParser::checkBlock(std::stack<std::string> &blockTokens)
+{
+	std::map<std::string, void (*)(std::stack<std::string> &)>::iterator res;
+	std::string token = tokens.top();
+	try
+	{
+		res = blockNames.find(token);
+		if (res == blockNames.end())
+		{
+			blockProp = token;
+			tokens.pop();
+			token = tokens.top();
+			res = blockNames.find(token);
+			if (res == blockNames.end() && token != "}")
+				throw errorHandler(INVALID_INSTRUCTION, token);
+			else if (res == blockNames.end())
+				throw errorHandler(INVALID_INSTRUCTION, blockProp);
+			else
+				res->second(blockTokens);
+		}
+		else
+			res->second(blockTokens);
+		tokens.pop();
+	}
+	catch (const std::exception &e)
+	{
+		throw errorHandler(std::string(e.what()));
+	}
+}
+
+void configParser::parseList(std::stack<std::string> &blockTokens)
+{
+	std::string token = tokens.top();
+	while (!tokens.empty())
+	{
+		if (token == "[")
+		{
+			blockTokens.push(token);
+			tokens.pop();
+			return ;
+		}
+		blockTokens.push(token);
+		tokens.pop();
+		token = tokens.top();
+	}
+	throw errorHandler(MISSING_TOKEN, "[");
+}
+
+ void configParser::parseBlock(void)
+{
+	char	last;
+
+	std::stack<std::string> blockTokens;
+ 	std::string token;
+	initBlockNames();
+	try
+ 	{
+		startBlock();
+ 		token = tokens.top();
+ 		while (!tokens.empty() && token != "host")
+ 		{
+			last = token[token.size() - 1];
+			//std::cout << "token " << token << "last " << last << "\n";
+ 			if (last == ':' || last == ';')
+ 			{
+ 				blockTokens.push(token);
+				tokens.pop();
+ 			}
+ 			else if (last == ']')
+ 				parseList(blockTokens);
+ 			else if (last == '[')
+ 				throw errorHandler(MISSING_TOKEN, "]");
+			else if (last == '}')
+ 			{
+ 				if(blockEnd)
+				 	throw errorHandler(MISSING_TOKEN, "{");
+ 				blockEnd = true;
+ 				tokens.pop();
+ 			}
+ 			else if (last == '{')
+			{
+				if(blockEnd == false && !blockTokens.empty())
+				{
+					throw errorHandler(MISSING_TOKEN, "}");
+				}
+ 				tokens.pop();
+ 				if (tokens.top() != "host")
+					checkBlock(blockTokens);
+ 				else
+ 					tokens.pop();
+ 				blockEnd = false;
+ 			}
+			else
+				throw errorHandler(INVALID_INSTRUCTION, token);
+ 			if (!tokens.empty())
+				token = tokens.top();
+		}
+		if (!tokens.empty())
+		{
+			std::cout << "token not empty " << tokens.top() << std::endl;
+			throw errorHandler(MISSING_TOKEN, "{");
+		}
+ 	}
+ 	catch (const std::exception &e)
+ 	{
+ 		throw errorHandler(std::string(e.what()));
+ 	}
+ }
+
+void configParser::initBlockNames(void)
+{
+	blockNames["host_configs"] = fillHostConf;
+	blockNames["error_pages"] = fillErrPg;
+	blockNames["route"] = fillRoute;
+	blockNames["cgi_config"] = fillCgiConf;
+	blockNames["location"] = fillLoc;
+}
+
+void configParser::parseConfig(std::string confFile)
+{
 	try
 	{
 		checkExt(confFile);
@@ -106,10 +292,11 @@ std::vector<serverConfig> configParser::parseConfig(std::string confFile)
 			throw errorHandler(WRONG_FILE, confFile);
 		flatten(file);
 		tokenize();
+		while (!tokens.empty())
+			parseBlock();
 	}
-	catch(const std::exception& e)
+	catch (const std::exception &e)
 	{
 		throw errorHandler(std::string(e.what()));
 	}
-	return res;
 }
